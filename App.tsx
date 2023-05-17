1045eb65f77ce9739507def72a93fb4d4b0e1209
raihanmuhammad <michael.raihan@stockbit.com>
2023-05-17 22:59:52 +0700

    init project and setup husky

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+const a = "a";
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
