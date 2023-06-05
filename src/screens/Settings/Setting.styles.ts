f1fbbd6ee9613b719f7a21b2618e626d48ad50a4
raihan-muhammad <sukasukangoding@gmail.com>
2023-06-06 01:45:36 +0700

    feat: add ui for settings page

--- src/screens/Settings/Setting.styles.ts
+++ src/screens/Settings/Setting.styles.ts
@@ -1,7 +1,37 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 40 
+    },
+    field: {
+        marginBottom: 30,
+        position: "relative"
+    },
+    label: {
+        fontSize: 18
+    },
+    choices: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        marginTop: 10,
+        marginHorizontal: -5
+    },
+    choice: {
+        backgroundColor: "#bbb",
+        padding: 10,
+        margin: 5
+    },
+    choiceText: {
+        color: "#fff"
+    },
+    switchField: {
+        flexDirection: "row",
+        justifyContent:"space-between",
+        alignItems: "center",
+        marginBottom: 0
+    }
 })
 
 export default styles;
--- src/screens/Settings/Setting.tsx
+++ src/screens/Settings/Setting.tsx
@@ -1,14 +1,57 @@
 
-import { ScrollView } from "react-native";
+import { ScrollView, Switch, TouchableOpacity, View } from "react-native";
 import { Text } from "components";
 import styles from "./Setting.styles";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 
 export default function Settings(): ReactElement {
+    const [state, setState] = useState<boolean>(true)
+    const difficulties = {
+        "1": "Beginner",
+        "3": "Intermediate",
+        "4": "Hard",
+        "-1": "Impossible"
+    }
     return (
         <ScrollView style={styles.container}>
-            <Text>Settings</Text>
+            <View style={styles.field}>
+                <Text style={styles.label}>Bot Difficulty</Text>
+                <View style={styles.choices}>
+                    {Object.keys(difficulties).map((level) => {
+                        return (
+                            <TouchableOpacity style={styles.choice} key={level} >
+                                <Text style={styles.choiceText}>{difficulties[level as keyof typeof difficulties]}</Text>
+                            </TouchableOpacity>
+                        )
+                    })}
+                </View>
+            </View>
+
+            <View style={[styles.field, styles.switchField]}>
+                <Text style={styles.label}>Sounds</Text>
+                <Switch 
+                    trackColor={{
+                        true: "#1B9C85"
+                    }}
+                    thumbColor="#fff"
+                    ios_backgroundColor="#1B9C85"
+                    value={state} 
+                    onValueChange={() => setState(!state)} 
+                />
+            </View>
+            <View style={[styles.field, styles.switchField]}>
+                <Text style={styles.label}>Vibrations</Text>
+                <Switch 
+                    trackColor={{
+                        true: "#1B9C85"
+                    }}
+                    thumbColor="#fff"
+                    ios_backgroundColor="#1B9C85"
+                    value={state} 
+                    onValueChange={() => setState(!state)} 
+                />
+            </View>
         </ScrollView>
     )
 }
