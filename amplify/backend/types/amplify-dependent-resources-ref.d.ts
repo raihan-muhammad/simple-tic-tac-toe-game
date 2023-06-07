c7ecf2c83a7302403d7a7e2a3962443c37d0a4b0
raihan-muhammad <sukasukangoding@gmail.com>
2023-06-07 23:04:57 +0700

    feat: setup configure and integrate with aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "simpletictactoegame": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "simpletictactoegame7f24e856": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import { AppBootstrap } from "components";
 import { ReactElement } from "react";
 import Navigator from "config/Navigator";
-import {SettingsProvider} from "contexts/SettingContext";
+import { SettingsProvider } from "contexts/SettingContext";
+import { Amplify } from "aws-amplify";
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 export default function App(): ReactElement {
   return (
