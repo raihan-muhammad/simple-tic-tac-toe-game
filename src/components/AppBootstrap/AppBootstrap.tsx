e033e9639a72b05c690cc0b6448e6a301c7883c9
raihan-muhammad <sukasukangoding@gmail.com>
2023-06-08 20:35:41 +0700

    fix: checking for logged users in app

--- src/components/AppBootstrap/AppBootstrap.tsx
+++ src/components/AppBootstrap/AppBootstrap.tsx
@@ -5,16 +5,30 @@ import {
 } from "@expo-google-fonts/delius-unicase";
 
 import AppLoading from "expo-app-loading";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { Auth } from "aws-amplify"
 
 type AppBootstrapProps = {
   children: ReactNode;
 };
 
 export default function AppBootstrap({ children }: AppBootstrapProps) {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
   const [fontLoaded] = useFonts({
     DeliusUnicase_400Regular,
     DeliusUnicase_700Bold,
   });
-  return fontLoaded ? <>{children}</> : <AppLoading />;
+  useEffect(() => {
+    async function checkCurrentUser(){
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        console.log(user);
+      } catch(err){
+        console.log(err);
+      }
+      setIsLogin(true);
+    }
+    checkCurrentUser();
+  }, []);
+  return fontLoaded && isLogin ? <>{children}</> : <AppLoading />;
 }
