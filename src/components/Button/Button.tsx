07ae107aaa24e8919b4d4da08510762df96015d2
raihan-muhammad <sukasukangoding@gmail.com>
2023-06-08 07:32:21 +0700

    feat: add login functionaly

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,30 @@
 import { ReactElement } from "react";
-import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from "react-native";
 import { Text } from "components";
 import styles from "./Button.styles";
 
 type ButtonProps = {
   title: string;
+  loading?: boolean;
+  textColor?: "white"
 } & TouchableOpacityProps;
 
 export default function Button({
   title,
   style,
+  textColor,
+  loading,
   ...props
 }: ButtonProps): ReactElement {
   return (
-    <TouchableOpacity style={[styles.button, style]} {...props}>
-      <Text style={styles.textButton} weight="bold">
-        {title}
-      </Text>
+    <TouchableOpacity disabled={loading} style={[styles.button, style]} {...props}>
+      {loading ? (
+        <ActivityIndicator/>
+      ): (
+        <Text style={[styles.textButton, { color: textColor === "white" ? "#fff" : "intial" }]} weight="bold">
+          {title}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 }
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,14 +1,74 @@
-import { ReactElement, useRef } from "react";
-import { ScrollView, TextInput as NativeTextInput } from "react-native";
-import { TextInput} from "components"
+import { ReactElement, useRef, useState } from "react";
+import { ScrollView, TextInput as NativeTextInput, Alert } from "react-native";
+import { TextInput, Button } from "components"
+import { Auth } from "aws-amplify";
 import styles from "./Login.styles";
 
 export default function Login(): ReactElement {
     const passwordRef = useRef<NativeTextInput | null>(null);
+    const [loading, setLoading] = useState(false)
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
+    })
+
+    const setFormInput = (key: keyof typeof form, value: string) => {
+        setForm({ ...form, [key]: value})
+    }
+
+    // const signup = async () => {
+    //     try {
+    //         const res = await Auth.signUp({
+    //             username: "test",
+    //             password: "testtest",
+    //             attributes: {
+    //                 email: "test@test.com",
+    //                 name: "test test"
+    //             }
+    //         })
+    //         console.log(res)
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+    // }
+
+    const Login = async () => {
+        setLoading(true)
+        try {
+            const { username, password} = form;
+            const res = await Auth.signIn(username, password);
+            console.log(res);
+        } catch(err){
+            if(err instanceof Error) Alert.alert("Error!", err.message || "An error has occurred!")
+        }
+        setLoading(false);
+    }
+
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <TextInput placeholder="Username" returnKeyType="next" onSubmitEditing={() => passwordRef.current?.focus() } />
-            <TextInput ref={passwordRef} placeholder="Username" returnKeyType="done" secureTextEntry />
+            <TextInput 
+                placeholder="Username" 
+                returnKeyType="next" 
+                onSubmitEditing={() => passwordRef.current?.focus() } 
+                value={form.username}
+                onChangeText={(value) => setFormInput("username", value)}
+            />
+            <TextInput 
+                ref={passwordRef} 
+                placeholder="Password" 
+                returnKeyType="done" 
+                secureTextEntry
+                value={form.password}
+                onChangeText={(value) => setFormInput("password", value)}
+            />
+
+            <Button 
+                loading={loading} 
+                title="Login" 
+                onPress={Login}
+                style={{ backgroundColor: "#1B9C85" }} 
+                textColor="white"
+            />
         </ScrollView>
     )
 }
